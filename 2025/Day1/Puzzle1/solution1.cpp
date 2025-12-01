03e53d1defb8aecb9939f1f0d6f11d2655f4b4bc
Kenzie Gibson <gibsonkenzie76@gmail.com>
2025-12-01 18:24:56 +0000

    Added cyclic logic and solved the puzzle
    The modulo operator is the goat in c++ as well it seems.  I really enjoyed figuring out the logic on this one, slightly harder than it initially appears! I feel ive learn't a good basis of the language from this puzzle alone! Although I know now alot more about what I don't know and have yet to learn!

--- 2025/Day1/Puzzle1/solution1.cpp
+++ 2025/Day1/Puzzle1/solution1.cpp
@@ -2,6 +2,7 @@
 //#include <ifstream> // for reading files
 #include <fstream> // for reading and writing to files
 #include <string>
+#include <typeinfo> // for checking variable types
 
 //#define max_lines 1000 // i dont like this hard coding method but ill do it for now
 
@@ -36,6 +37,9 @@ int main() {
 	int current_position;
 	current_position = 50;
 
+	// track how many zeros the dial lands on after a move
+	int zeros = 0;
+
 	while (getline(file, line)) // while there are lines from file to read
 	{	
 		//cout << line[0] << endl; // print each line plus start a new line
@@ -48,12 +52,56 @@ int main() {
 		no_moves = line.substr(1);
 
 		// \n prints new line without flushing buffer
-		cout << direction << "\n" << no_moves << endl;
+		//cout << direction << "\n" << no_moves << endl;
 
+		// convert number of moves into integer
+		int no_moves_int = std::stoi(no_moves);
+
+		// check integer conversion was successful
+		//std::cout << typeid(no_moves_int).name() << '\n'; // need #include <typeinfo>
+
+		// can just do this when doing the directional if statements, saves lines
+		/*
+		// find number of moves equivalent to moving just once around the dial
+		if (no_moves_int > 99)
+		{
+			no_moves_int = no_moves_int % 99; // modulo is the goat in all languages
+		}*/
+
+		// check all moves are now less than 99
+		//std::cout << no_moves_int << endl;
+
+		// if dial must move to the right
+		if (direction == "R") 
+		{
+			current_position = (current_position + no_moves_int)%100; // as there are 100 positions including 0
+		}
+
+		// if dial must move to the left
+		if (direction == "L")
+		{
+			current_position = (current_position - no_moves_int)%100;
+
+			// fix any negative values
+			if (current_position < 0) 
+			{
+				current_position += 100;
+			}
+		}
+
+		//cout << current_position << endl;
+
+		if (current_position == 0) 
+		{
+			zeros += 1; // had it as =+ before which assigns the variable as the positive value of the number, explaining why my solution was 1 before
+		}
+		
 
 	}
 
+	cout << "Number of times dial lands on zero: " << zeros;
 
 
-	return 0; // its convention to return 0 for successfully run code in c++ so the OS knows that it ran successfully
-}
+	// its convention to return 0 for successfully run code in c++ so the OS knows that it ran successfully
+	return 0;
+	}
