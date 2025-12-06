6f7c91ce9f4b3948573e18e62d7d2abde2624c16
Kenzie Gibson <gibsonkenzie76@gmail.com>
2025-12-06 12:36:35 +0000

    Joltage found for test number
    Still need to use input and loop through numbers, but method is now sound and tested.
    
    I needed to switch some int var types to long long when testing larger numbers as the int 32 bit wont work for the inputs.

--- 2025/Day3/Puzzle_1/Puzzle_1.cpp
+++ 2025/Day3/Puzzle_1/Puzzle_1.cpp
@@ -6,7 +6,7 @@
 // number is not modified outside the original function
 int find_max_value(std::string number_str) {
 
-	int number = std::stoi(number_str);
+	long long number = std::stoi(number_str);
 
 	int max_digit = 0; //use to store max digit when looping through number later
 
@@ -14,7 +14,7 @@ int find_max_value(std::string number_str) {
 	while (number > 0) {
 
 		// isolate final digit in the number
-		int remainder = number % 10;
+		long long remainder = number % 10;
 
 		if (remainder > max_digit) {
 			max_digit = remainder; // remainder is the largest digit thus far
@@ -30,6 +30,9 @@ int find_max_value(std::string number_str) {
 	// print the maximum digit
 	std::cout << "Max digit is " << max_digit << "\n";
 
+	// convert to string 
+	//std::string max_digit_str = std::to_string(max_digit);
+
 	return max_digit;
 
 }
@@ -40,13 +43,35 @@ int find_max_value(std::string number_str) {
 int main() {
 
 	// string as this is what the input will be
-	std::string test = "834921";
+	std::string test = "994783256"; // tested larger values, reminded me about the long long variable type to replace int so that the larger numbers in the input work
 
 	// find the largest digit in the number
 	int max_digit = find_max_value(test);
-	std::cout << max_digit;
+	std::string max_digit_str = std::to_string(max_digit);
+	//std::cout << max_digit;
 
 	// find index of first occurance of the number
+	int index = test.find(max_digit_str); // get first occurance of the max digit in the number
+
+	// slice number from that index to the end
+	//int len = test.size();
+
+	std::cout << test << "\n" << max_digit << "\n" << index << "\n";
+
+	// cut number from max_digit + 1 to end
+	std::string splice = test.substr(index + 1);
+	//std::cout << splice; // check splice is done as expected
+
+	// get largest value after the occurance of the first value
+	int second_digit = find_max_value(splice);
+	std::string second_digit_str = std::to_string(second_digit);
+
+	std::string joltage = max_digit_str + second_digit_str;
+
+	std::cout << joltage;
+
+
+
 
 
 
