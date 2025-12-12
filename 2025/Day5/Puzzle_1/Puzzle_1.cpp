65f4346a14cf8a9cb2b729db37cc2639316c7770
Kenzie Gibson <gibsonkenzie76@gmail.com>
2025-12-12 21:10:34 +0000

    Pulled ID ranges from input file

--- 2025/Day5/Puzzle_1/Puzzle_1.cpp
+++ 2025/Day5/Puzzle_1/Puzzle_1.cpp
@@ -1,8 +1,42 @@
-#include <fstream>
+#include <fstream> //reading files
 #include <iostream> // for cout
+#include <string> //getline
+
 
 // function that is run when script built and ran, int tells us about the integer type return? (!!! Check this)
-bool main() {
+int main() {
+
+	// read in input file
+	std::string filename = "input.txt";
+	std::ifstream file; // create ifstream item, allows file to be connected from the stream
+	file.open(filename);
+
+	// check file opened 
+	if (file.fail()) {
+	
+		std::cout << "Could not read file" << std::endl;
+
+		return 1; // lets OS know that program didnt run correctly
+	}
+
+	// store single range at a time as read in from the input file
+	std::string range;
+
+	// read in ranges
+	while (std::getline(file, range)) {
+
+		std::cout << range << std::endl;
+		
+		// check to see if blank line present (end of ranges)
+		if (!range.size()) {
+		
+			break;
+		}
+	
+	}
+
+
+
 
 	// testing logic for determining if test value is between range down - up.
 	long long test = 238330044306852;
